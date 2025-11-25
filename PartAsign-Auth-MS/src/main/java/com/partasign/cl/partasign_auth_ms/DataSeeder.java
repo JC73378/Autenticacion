6e567cc507af06b69c4d89e68c2f087032f402a2
JC <ju.carmonag@duocuc.cl>
2025-11-24 22:25:55 -0300

    cambios completados

--- PartAsign-Auth-MS/src/main/java/com/partasign/cl/partasign_auth_ms/DataSeeder.java
+++ PartAsign-Auth-MS/src/main/java/com/partasign/cl/partasign_auth_ms/DataSeeder.java
@@ -3,11 +3,13 @@ package com.partasign.cl.partasign_auth_ms;
 import com.partasign.cl.partasign_auth_ms.Model.Usuario;
 import com.partasign.cl.partasign_auth_ms.Repository.UsuarioRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
+@Profile("dev")
 public class DataSeeder implements CommandLineRunner {
 
     private final UsuarioRepository repository;
